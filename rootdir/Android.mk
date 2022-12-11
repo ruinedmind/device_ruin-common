18cc55039fde979dbee54307d463fc63182c4f57
ruinedmind <119005547+ruinedmind@users.noreply.github.com>
2022-12-11 15:00:20 +0530

    rootdir: Update Files
    Extracted From Stock

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -190,3 +190,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.qcom.early_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.m51.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.m51.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
